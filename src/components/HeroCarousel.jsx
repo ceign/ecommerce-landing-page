ba4fea94af1856f23f79633c96f16de7e184f2bd
ceign <dnssdvlp@gmail.com>
2023-10-29 01:09:08 -0300

    Work in carousel hero. Add keen slider lib.

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 import "../App.css";
 import "./HeroCarousel.css";
 import Carousel from "../assets/hero-carousel.jpeg";
@@ -17,15 +19,53 @@ const HeroCarousel = () => {
     }
   }, []);
 
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+    },
+    [
+      (slider) => {
+        let timeout
+        let mouseOver = false
+        function clearNextTimeout() {
+          clearTimeout(timeout)
+        }
+        function nextTimeout() {
+          clearTimeout(timeout)
+          if (mouseOver) return
+          timeout = setTimeout(() => {
+            slider.next()
+          }, 3000)
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true
+            clearNextTimeout()
+          })
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false
+            nextTimeout()
+          })
+          nextTimeout()
+        })
+        slider.on("dragStarted", clearNextTimeout)
+        slider.on("animationEnded", nextTimeout)
+        slider.on("updated", nextTimeout)
+      },
+    ]
+  )
+
   return (
     <>
       {showNavigation && (
         <div className="hero__carousel">
           <div className="hero__carousel-wrapper">
-            <figure>
-              <img src={Carousel} alt="" />
-              <span></span>
+            <figure ref={sliderRef} className="keen-slider">
+              <img className="keen-slider__slide number-slide1" src={Carousel} alt="" />
+              <img className="keen-slider__slide number-slide2" src={Carousel} alt="" />
+              <img className="keen-slider__slide number-slide3" src={Carousel} alt="" />
             </figure>
+            <span></span>
           </div>
         </div>
       )}
