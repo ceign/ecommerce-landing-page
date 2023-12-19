0fdf48c57c0f9bfe7aed2a54b043d5c011f5c89e
ceign <dnssdvlp@gmail.com>
2023-12-19 20:11:40 -0300

    Fix image paths.

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,14 +1,21 @@
 import "./Partners.css";
+import Partner1 from "../assets/partner-1.svg";
+import Partner2 from "../assets/partner-2.svg";
+import Partner3 from "../assets/partner-3.svg";
+import Partner4 from "../assets/partner-4.svg";
+import Partner5 from "../assets/partner-5.svg";
+import Partner6 from "../assets/partner-6.svg";
+
 
 const Partners = () => {
 
   const partnertImages = [
-    "/src/assets/partner-1.svg",
-    "/src/assets/partner-2.svg",
-    "/src/assets/partner-3.svg",
-    "/src/assets/partner-4.svg",
-    "/src/assets/partner-5.svg",
-    "/src/assets/partner-6.svg",
+    Partner1,
+    Partner2,
+    Partner3,
+    Partner4,
+    Partner5,
+    Partner6,
   ];
 
   return (
